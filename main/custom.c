16c63567359f8b287d8cf24f74e18bfe3017d13a
karawin <jp@cocatrix.fr>
2018-10-31 23:16:54 +0100

    improved custom.c

--- main/custom.c
+++ main/custom.c
@@ -2,11 +2,21 @@
  * 
  * Copyright 2018 karawin (http://www.karawin.fr)
  *
+ *
+ * This file contain empty functions definition on the standard delivery
 *******************************************************************************/
+#include "driver/gpio.h"
+#include "gpio.h"
+#include "custom.h"
 
+#define gpioLedBacklight 5
+
+// LedBacklight control a gpio to switch on or off the lcd backlight 
+// in addition of the sys.lcdout("x")  configuration for system with battries.
+// added by Vadim Polyakovsky 
 void LedBacklightInit()
 {
-	// gpio_output_conf(gpioLedBacklight)
+	//gpio_output_conf(gpioLedBacklight);
 	//gpio_set_level(gpioLedBacklight,1);
 }	 
  
