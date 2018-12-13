d3a81496ec4e8c1772d51dd67a0e21e2876e6bc1
karawin <jp@cocatrix.fr>
2018-12-13 15:44:04 +0100

    nvs_partition_generator.sh changed

--- boards/alt_nvs_partition_generator.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-filename=$(basename "$1")
-fname="${filename%.*}"
-echo python ${IDF_PATH}/components/nvs_flash/nvs_partition_generator/nvs_partition_gen.py ${fname}.csv build/${fname}.bin 8
-python ${IDF_PATH}/components/nvs_flash/nvs_partition_generator/nvs_partition_gen.py ${fname}.csv build/${fname}.bin 8
-echo done
