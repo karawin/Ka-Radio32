6ea9b1746a873db8b8c2c805055604d13baf4b84
karawin <jp@cocatrix.fr>
2020-01-11 19:54:15 +0100

    Release 1.9 R6  vs1053.c was wrong.

--- main/vs1053.c
+++ main/vs1053.c
@@ -118,7 +118,7 @@ bool VS1053_HW_init()
 		.address_bits = 8,
 		.dummy_bits = 0,
 		.duty_cycle_pos = 0,
-		.cs_ena_pretrans = 1,
+		.cs_ena_pretrans = 0,
 		.cs_ena_posttrans = 1,
 		.flags = 0,	
         .mode=0,                         //SPI mode 
@@ -144,7 +144,7 @@ bool VS1053_HW_init()
 	//Initialize non-SPI GPIOs
 	gpio_config_t gpio_conf;
 	gpio_conf.mode = GPIO_MODE_OUTPUT;
-	gpio_conf.pull_up_en =  GPIO_PULLUP_ENABLE;
+	gpio_conf.pull_up_en =  GPIO_PULLUP_DISABLE;
 	gpio_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
 	gpio_conf.intr_type = GPIO_INTR_DISABLE;	
 	gpio_conf.pin_bit_mask = ((uint64_t)(((uint64_t)1)<<rst));
